20309237fc108ce4102142d6906d1dd4ddfb601b
Konstantinos <kdalkafoukis@yahoo.gr>
2018-09-07 12:29:22 +0100

    adding conditional rendering for mobile or desktop version

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
-import {projects} from '../Data/projectData.js';
-import '../Styles/SideBarStyle.css';
-import '../Styles/App.css';
-import PersonalPage from './PersonalPage';
-import Project from './Project';
-import FullScreenPage from './FullScreenPage';
-import NotFound from './NotFound';
-import Contact from './Contact';
-
-
-const active = {color: '#878787'};
+import Media from 'react-media';
+import Desktop from './Desktop';
+import Mobile from './Mobile';
 
 const App = () => {
   return (
-    <Router>
-      <div className='App'>
-        <div className='SideBar'>
-          <NavLink className='Name' to='/'>MARIOS TSIPOPOULOS</NavLink>
-          {projects.map((project, index) =>
-          <NavLink key={index} activeStyle={active} className='Project' to={`/${project}`} >Project {index+1}</NavLink>
-          )}
-          <NavLink activeStyle={active} className='Project' to='/contact' >Contact</NavLink>
-        </div>
-        <Switch>
-          <Route exact path='/' component={PersonalPage} />
-          {projects.map((project, index) =>
-            <Route key={index} exact path={`/${project}/:id`} render={(props)=><FullScreenPage {...props} project={`/${project}`}/>} />
-          )}
-          {projects.map((project, index) =>
-            <Route key={index} exact path={`/${project}`} render={(props)=><Project {...props} project={`/${project}`}/>} />
-          )}
-          <Route exact path='/contact' component={Contact} />
-          <Route component={NotFound} />
-        </Switch >
-      </div>
-    </Router>
+    <Media query="(max-width: 799px)">
+      {matches =>
+        matches ?
+          <Mobile/>
+          :
+          <Desktop/>
+      }
+    </Media>
   );
 }
 
---
+++ src/Components/Mobile.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
+import {projects} from '../Data/projectData.js';
+import '../Styles/SideBarStyle.css';
+import '../Styles/App.css';
+import PersonalPage from './PersonalPage';
+import Project from './Project';
+import NotFound from './NotFound';
+import Contact from './Contact';
+
+const active = {color: '#878787'};
+
+const Mobile = () => (
+  <Router>
+    <div className='App'>
+      <div className='SideBar'>
+        <NavLink className='Name' to='/'>MARIOS TSIPOPOULOS</NavLink>
+        {projects.map((project, index) =>
+        <NavLink key={index} activeStyle={active} className='Project' to={`/${project}`} >Project {index+1}</NavLink>
+        )}
+        <NavLink activeStyle={active} className='Project' to='/contact' >Contact</NavLink>
+      </div>
+      <Switch>
+        <Route exact path='/' component={PersonalPage} />
+        {projects.map((project, index) =>
+          <Route key={index} exact path={`/${project}/:id`} render={(props)=><Project {...props} project={`/${project}`}/>} />
+        )}
+        {projects.map((project, index) =>
+          <Route key={index} exact path={`/${project}`} render={(props)=><Project {...props} project={`/${project}`}/>} />
+        )}
+        <Route exact path='/contact' component={Contact} />
+        <Route component={NotFound} />
+      </Switch >
+    </div>
+  </Router>
+);
+
+export default Mobile;
